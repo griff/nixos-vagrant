d506c9831eeb5ca3dbfee1822718db97caecd5f8
Brian Olsen <brian@maven-group.org>
2017-05-24 22:07:18 +0200

    Make autoupdate script

---
+++ find-version.sh
@@ -0,0 +1,7 @@
+
+export NIXOS_VERSION="17.03.$(curl https://nixos.org/nixos/download.html | sed  -n -E "s/.*<strong>17.03.(.+)<.strong>.*/\1/p")"
+export NIXOS_CHECKSUM="$(curl -L "https://d3g5gsiof5omrk.cloudfront.net/nixos/17.03/nixos-${NIXOS_VERSION}/nixos-minimal-${NIXOS_VERSION}-x86_64-linux.iso.sha256")"
+echo "Nixos: ${NIXOS_VERSION} ${NIXOS_CHECKSUM}"
+
+sed -i '' -e "s#\"nixos_version\": \".*\",#\"nixos_version\": \"${NIXOS_VERSION}\",#" nixos.json
+sed -i '' -e "s#\"nixos_checksum\": \".*\",#\"iso_checksum\": \"${NIXOS_CHECKSUM}\",#" nixos.json
