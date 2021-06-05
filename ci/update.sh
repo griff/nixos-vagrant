819901386bec2c6cf5e001d2f9bc6b5eb12c5740
Brian Olsen <brian@maven-group.org>
2021-06-05 15:23:14 +0200

    Updates for 21.05

--- ci/update.sh
+++ ci/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./find-version.sh write 19.09 > nixos-19.09-var.json
 ./find-version.sh write 20.03 > nixos-20.03-var.json
 ./find-version.sh write 20.09 > nixos-20.09-var.json
+./find-version.sh write 21.05 > nixos-21.05-var.json
 ./find-version.sh write stable > nixos-stable-var.json
