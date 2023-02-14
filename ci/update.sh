088e353ae4514eb8ffee2c72172cc64d930f9c35
Brian Olsen <brian@maven-group.org>
2023-02-14 15:37:34 +0100

    Upgrade to latest NixOS

--- ci/update.sh
+++ ci/update.sh
@@ -3,4 +3,7 @@
 ./find-version.sh write 20.03 > nixos-20.03-var.json
 ./find-version.sh write 20.09 > nixos-20.09-var.json
 ./find-version.sh write 21.05 > nixos-21.05-var.json
+./find-version.sh write 21.11 > nixos-21.11-var.json
+./find-version.sh write 22.05 > nixos-22.05-var.json
+./find-version.sh write 22.11 > nixos-22.11-var.json
 ./find-version.sh write stable > nixos-stable-var.json
